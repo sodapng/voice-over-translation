985c027cb33ddd25559ff7b4023e9a3b3a9dded8
ilyhalight <ilya.t24@yandex.ru>
2022-01-23 02:28:27 +0300

    v1.0.1 | Файл для созадния браузера

---
+++ web-server/browser.js
@@ -0,0 +1,38 @@
+import puppeteer from 'puppeteer-core' 
+import config from 'config'
+
+const args = [
+ '--no-zygote',
+ '--no-first-run',
+ '--ignore-certificate-errors',
+ '--ignore-certificate-errors-skip-list',
+ '--disable-accelerated-2d-canvas',
+ '--hide-scrollbars',
+ '--disable-notifications',
+ '--disable-background-timer-throttling',
+ '--disable-backgrounding-occluded-windows',
+ '--disable-breakpad',
+ '--disable-extensions',
+ '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+ '--disable-ipc-flooding-protection',
+ '--metrics-recording-only',
+ '--mute-audio',
+ '--remote-debugging-port=12703',
+]
+
+// Не работает с headless: true
+const options = {
+ headless: false, 
+ ignoreHTTPSErrors: true,
+ executablePath: config.get('executablePath'),
+ userDataDir: config.get('userDataDir'),
+ args,
+}
+
+async function createBrowser () {
+    const browser = await puppeteer.launch(options)
+    const wsEndpoint = await browser.wsEndpoint()
+    console.log(wsEndpoint)
+}
+
+export default createBrowser
