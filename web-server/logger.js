f52ecfec31c85a22e0a21124fd8f84e72fc9bbf8
Toil <ilya.t24@yandex.ru>
2022-04-16 01:13:14 +0300

    Добавлено нормальное логирование для консоли
    Добавлено нормальное логирование для консоли с возможностью настройки некоторых параметров

---
+++ web-server/logger.js
@@ -0,0 +1,40 @@
+import winston from 'winston';
+import config from 'config'
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} | ${level}: ${message}`;
+});
+
+const getTransport = async () => {
+  if (config.get('saveLogs') === true) {
+    return [
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'all.log' }),
+      new winston.transports.Console(),
+    ]
+  } else {
+    return [
+      new winston.transports.Console(),
+    ]
+  }
+}
+
+const getLogsLevel = async () => {
+  return config.get('debugLogs') === true ? 'debug' : 'info'
+}
+
+const logger = winston.createLogger({
+  level: await getLogsLevel(),
+  exitOnError: false,
+  format: combine(
+    winston.format.colorize(),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    myFormat
+  ),
+  defaultMeta: { service: 'translation' },
+  transports: await getTransport()
+})
+
+export default logger
