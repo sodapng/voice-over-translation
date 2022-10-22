daf4bd7e93b6bbcae4910792aa61b868451cba7f
Toil <toil.official@yandex.com>
2022-10-23 02:39:10 +0300

    changed ext version

--- vot.user.js
+++ vot.user.js
@@ -3,7 +3,7 @@
 // @name:ru          [VOT] - Закадровый перевод видео
 // @description      A small extension that adds a Yandex Browser video translation to other browsers
 // @description:ru   Небольшое расширение, которое добавляет закадровый перевод видео из Яндекс Браузера в другие браузеры
-// @version          1.0.9
+// @version          1.0.9.1
 // @author           sodapng, mynovelhost, Toil
 // @match            *://*.youtube.com/*
 // @match            *://*.twitch.tv/*
