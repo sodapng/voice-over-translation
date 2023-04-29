be55766ca558435d0d3b86a9a8bfafb2b1ed4a09
SashaXser <24498484+SashaXser@users.noreply.github.com>
2023-04-29 20:00:50 +0400

    Update getTranslate.js

--- vot-cli/getTranslate.js
+++ vot-cli/getTranslate.js
@@ -1,7 +1,6 @@
 import protobuf from 'protobufjs';
 import crypto from 'crypto';
 import axios from 'axios';
-import chalk from 'chalk';
 
 const yandexHmacKey = "gnnde87s24kcuMH8rbWhLyfeuEKDkGGm";
 const yandexUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 CriOS/104.0.5112.114 YaBrowser/22.9.4.633.10 SA/3 Mobile/15E148 Safari/604.1";
