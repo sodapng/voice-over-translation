0f220121b088a834364c86593d8921fde1a2ce75
unknown <toil.official@yandex.com>
2023-05-23 02:04:38 +0300

    constants & urls moved to config

---
+++ src/config/alternativeUrls.js
@@ -0,0 +1,52 @@
+// Sites host Invidious. I tested the performance only on invidious.kevin.rocks, youtu.be and inv.vern.cc
+const sitesInvidious = [
+  'invidious.snopyta.org',
+  'yewtu.be',
+  'invidious.kavin.rocks',
+  'vid.puffyan.us',
+  'invidious.namazso.eu',
+  'inv.riverside.rocks',
+  'yt.artemislena.eu',
+  'invidious.flokinet.to',
+  'invidious.esmailelbob.xyz',
+  'y.com.sb',
+  'invidious.nerdvpn.de',
+  'inv.vern.cc',
+  'invidious.slipfox.xyz',
+  'invidio.xamh.de',
+  'invidious.dhusch.de'
+];
+
+// Sites host Piped. I tested the performance only on piped.video
+const sitesPiped = [
+  'piped.video',
+  'piped.tokhmi.xyz',
+  'piped.moomoo.me',
+  'piped.syncpundit.io',
+  'piped.mha.fi',
+  'watch.whatever.social',
+  'piped.garudalinux.org',
+  'efy.piped.pages.dev',
+  'watch.leptons.xyz',
+  'piped.lunar.icu',
+  'yt.dc09.ru',
+  'piped.mint.lgbt',
+  'il.ax',
+  'piped.privacy.com.de',
+  'piped.esmailelbob.xyz',
+  'piped.projectsegfau.lt',
+  'piped.in.projectsegfau.lt',
+  'piped.us.projectsegfau.lt',
+  'piped.privacydev.net',
+  'piped.palveluntarjoaja.eu',
+  'piped.smnz.de',
+  'piped.adminforge.de',
+  'piped.qdi.fi',
+  'piped.hostux.net',
+  'piped.chauvet.pro',
+  'piped.jotoma.de',
+  'piped.pfcd.me',
+  'piped.frontendfriendly.xyz'
+];
+
+export { sitesInvidious, sitesPiped }
---
+++ src/config/constants.js
@@ -0,0 +1,20 @@
+const translateFuncParam = 0x40_75_50_00_00_00_00_00;
+const availableFromLangs = {'ru': 'Русский', 'en': 'Английский', 'zh': 'Китайский', 'fr': 'Французский', 'it': 'Итальянский', 'es': 'Испанский'}; // available languages for translation (from)
+const availableToLangs = {'ru': 'Русский', 'en': 'Английский'}; // available languages for translation (to)
+const siteTranslates = {
+  'youtube': 'https://youtu.be/',
+  'twitch': 'https://twitch.tv/',
+  'vimeo': 'https://vimeo.com/',
+  '9gag': 'https://9gag.com/gag/',
+  'vk': 'https://vk.com/video?z=',
+  'xvideos': 'https://www.xvideos.com/',
+  'pornhub': 'https://rt.pornhub.com/view_video.php?viewkey=',
+  'udemy': 'https://www.udemy.com',
+  'twitter': 'https://twitter.com/i/status/',
+  'facebook': 'https://www.facebook.com/',
+  'rutube': 'https://rutube.ru/video/',
+  'bilibili.com': 'https://www.bilibili.com/video/'
+};
+
+
+export { translateFuncParam, availableFromLangs, availableToLangs, siteTranslates }
