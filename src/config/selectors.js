2492e54b5f0b90278c61ddffd8227137b4278cc9
unknown <toil.official@yandex.com>
2023-05-23 00:54:09 +0300

    important selectors moved from main script to config

---
+++ src/config/selectors.js
@@ -0,0 +1,16 @@
+const selectors = () => {
+  return {
+    'youtubeSelector':  '.html5-video-container',
+    'twitchSelector': '.video-ref',
+    'twitchMobileSelector': 'main > div > section > div > div > div',
+    'pipedSelector': '.player-container',
+    'vkSelector': '.videoplayer_media',
+    'twitterSelector': 'article[data-testid="tweet"][tabindex="-1"]',
+    'vimeoSelector': '.player',
+    'gagSelector': '.video-post',
+    'bilibilicomSelector': '.bpx-player-video-wrap'
+  }
+}
+
+export default selectors();
+
