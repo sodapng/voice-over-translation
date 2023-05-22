60dcb3c7414e0c097f1af23e488746d4346b0733
Dragoy <dragoy18@gmail.com>
2023-05-22 20:26:04 +0200

    Added possibility to specify several links at once

--- vot-cli/translate.ps1
+++ vot-cli/translate.ps1
@@ -1,10 +1,10 @@
 # Скрипт для PowerShell который скачивает видео, перевод и смешивает
 #
 # Использование:
-# .\translate.ps1 <ссылка на видео> [отношение громкости оригинала - например: 0.4]
+# .\translate.ps1 <ссылка на видео 1> <ссылка на видео 2> ... [отношение громкости оригинала - например: 0.4]
 # 
 # Usage:
-# .\translate.ps1 <video link> [volume ratio - for example: 0.4]
+# .\translate.ps1 <video link 1> <video link 2> ... [volume ratio - for example: 0.4]
 #
 # Буду рад узнать ваши идеи и о найденных ошибках!
 # any ideas/issues are welcome!!
@@ -12,22 +12,8 @@
 
 
 
-# Settings
-$original_sound_ratio = 0.1
-$temp_dir = "./temp" # will be removed
-$temp_video_dir = "$temp_dir/video"
-$temp_video = "$temp_video_dir/%(title)s.%(ext)s"
-$temp_audio = "$temp_dir/audio"
 
-$video_link = $args[0]
-# If has second argument, set the original sound ratio to that one
-if ($args[1]) {
-    $original_sound_ratio = $args[1]
-}
-
-# Check that var is init
-if ($video_link) {
-    Write-Host "Original volume is set to $original_sound_ratio"
+function ProcessVideo($video_link, $original_sound_ratio) {
     # No -Force, to make sure nothing is overwritten
     New-Item -ItemType Directory -Path $temp_dir -ErrorAction SilentlyContinue | Out-Null
     New-Item -ItemType Directory -Path $temp_video_dir -ErrorAction SilentlyContinue | Out-Null
@@ -35,8 +21,7 @@ if ($video_link) {
 
     yt-dlp -o $temp_video $video_link
     $video_full_name = Get-ChildItem $temp_video_dir
-    # Не забывайте сделать $env:Path += ";C:\Program Files\nodejs\" что бы в PowerShell работал npx!
-    npx vot-cli $video_link --output $temp_audio
+    vot-cli $video_link --output $temp_audio
 
     $temp_video_file = (Get-ChildItem -Path $temp_video_dir)[0].FullName
     $temp_audio_file = (Get-ChildItem -Path $temp_audio)[0].FullName
@@ -44,6 +29,7 @@ if ($video_link) {
     ffmpeg `
         -i $temp_video_file -i $temp_audio_file `
         -c:v copy `
+		-b:a 128k `
         -filter_complex " `
             [0:a] volume=$original_sound_ratio [original]; `
             [original][1:a] amix=inputs=2:duration=longest [audio_out] `
@@ -52,6 +38,38 @@ if ($video_link) {
         -y $video_full_name
 
     Remove-Item -Recurse -Force $temp_dir
+}
+
+# Settings
+$original_sound_ratio = 0.1
+$temp_dir = "./temp" # will be removed
+$temp_video_dir = "$temp_dir/video"
+$temp_video = "$temp_video_dir/%(title)s.%(ext)s"
+$temp_audio = "$temp_dir/audio"
+
+$video_links = $args[0..($args.Length - 2)]
+$volume_ratio_arg = $args[-1]
+
+# If the last argument is a number, set the original sound ratio to that one
+if ($volume_ratio_arg -as [double]) {
+    $original_sound_ratio = $volume_ratio_arg
+    Write-Host "Original volume is set to $original_sound_ratio"
 } else {
-    Write-Host "You didn't enter a link to the video"
+    # If the last argument is not a number, add it back to the video links array
+    $video_links += $volume_ratio_arg
+}
+
+# Check that var is init
+if ($video_links) {
+    foreach ($video_link in $video_links) {
+        if ([string]::IsNullOrEmpty($video_link)) {
+            Write-Host "Error: Link not entered."
+            continue
+        }
+        
+        Write-Host "Processing video: $video_link"
+        ProcessVideo $video_link $original_sound_ratio
+    }
+} else {
+    Write-Host "Error: Link not entered."
 }
