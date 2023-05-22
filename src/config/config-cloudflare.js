54f45e7b7f7a4956f3eeb4cf61516cb661c037db
unknown <toil.official@yandex.com>
2023-05-23 00:52:58 +0300

    removed ua

--- src/config/config-cloudflare.js
+++ src/config/config-cloudflare.js
@@ -1,5 +1,5 @@
+// CLOUDFLARE CONFIGURATION
 const workerHost = "cors.yandexproxy.workers.dev";
 const yandexHmacKey = "gnnde87s24kcuMH8rbWhLyfeuEKDkGGm";
-const yandexUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 CriOS/104.0.5112.114 YaBrowser/22.9.4.633.10 SA/3 Mobile/15E148 Safari/604.1";
 
-export { workerHost, yandexHmacKey, yandexUserAgent };
+export { workerHost, yandexHmacKey };
