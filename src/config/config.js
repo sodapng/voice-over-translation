c69d92a18fdeb2401bd1ca9cff2a206a1687d797
unknown <toil.official@yandex.com>
2023-05-23 00:53:19 +0300

    autoVolume moved from main script to config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
+// CONFIGURATION
 const workerHost = "api.browser.yandex.ru";
 const yandexHmacKey = "gnnde87s24kcuMH8rbWhLyfeuEKDkGGm";
 const yandexUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 CriOS/104.0.5112.114 YaBrowser/22.9.4.633.10 SA/3 Mobile/15E148 Safari/604.1";
+const autoVolume = 0.15; // 0.0 - 1.0 (0% - 100%) - default volume of the video with the translation
 
-export { workerHost, yandexHmacKey, yandexUserAgent };
+export { workerHost, yandexHmacKey, yandexUserAgent, autoVolume };
