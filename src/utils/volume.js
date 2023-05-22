3159b7ecf085b7a85d4bb305102e3ff4eb033f65
unknown <toil.official@yandex.com>
2023-05-23 02:04:22 +0300

    function for sync volume moved to volume.js

---
+++ src/utils/volume.js
@@ -0,0 +1,29 @@
+// element - audio / video element
+function syncVolume(element, sliderVolume, otherSliderVolume, tempVolume) {
+  let finalValue;
+  if (sliderVolume > tempVolume) {
+    // sliderVolume = 100
+    // tempVolume = 69
+    // volume = 15
+    // 100 - 69 = 31
+    // 15 + 31 = 46 - final video volume
+    finalValue = otherSliderVolume + (sliderVolume - tempVolume);
+    finalValue = finalValue > 100 ? 100 : Math.max(finalValue, 0);
+
+    element.volume = finalValue / 100;
+  } else if (sliderVolume < tempVolume) {
+    // sliderVolume = 69
+    // tempVolume = 100
+    // volume = 15
+    // 100 - 69 = 31
+    // 15 - 31 = 0 - final video volume
+    finalValue = otherSliderVolume - (tempVolume - sliderVolume);
+    finalValue = finalValue > 100 ? 100 : Math.max(finalValue, 0);
+
+    element.volume = finalValue / 100;
+  }
+
+  return finalValue
+}
+
+export { syncVolume };
