098538084a3f9cfa14824f2c5bcdf3f7d51182a7
unknown <toil.official@yandex.com>
2023-07-05 15:17:55 +0300

    changed worker host

--- src/config/config-cloudflare.js
+++ src/config/config-cloudflare.js
@@ -1,5 +1,5 @@
 // CLOUDFLARE CONFIGURATION
-const workerHost = "cors.yandexproxy.workers.dev";
+const workerHost = "vot.toil-dump.workers.dev";
 const yandexHmacKey = "gnnde87s24kcuMH8rbWhLyfeuEKDkGGm";
 
 export { workerHost, yandexHmacKey };
