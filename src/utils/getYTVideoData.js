0b0346ca476888470b89297779bfee8895b471c3
SashaXser <24498484+SashaXser@users.noreply.github.com>
2023-07-22 07:46:35 +0400

    Add files via upload

--- src/utils/getYTVideoData.js
+++ src/utils/getYTVideoData.js
@@ -6,15 +6,19 @@ async function detect(cleanText) {
   return await response.text();
 }
 
+// Get the language code from the response or the text
 async function getLanguage(player, response, title, description, author) {
   if (!window.location.hostname.includes("m.youtube.com")) {
+    // ! Experimental ! get lang from selected audio track if availabled
     const audioTracks = player.getAudioTrack();
-    const trackInfo = audioTracks?.getLanguageInfo();
+    const trackInfo = audioTracks?.getLanguageInfo(); // get selected track info (id === "und" if tracks are not available)
     if (trackInfo?.id !== "und") {
       return trackInfo.id.split(".")[0];
     }
   }
 
+  // TODO: If the audio tracks will work fine, transfer the receipt of captions to the audioTracks variable
+  // Check if there is an automatic caption track in the response
   const captionTracks =
     response?.captions?.playerCaptionsTracklistRenderer?.captionTracks;
   if (captionTracks?.length) {
@@ -23,7 +27,9 @@ async function getLanguage(player, response, title, description, author) {
       return autoCaption.languageCode;
     }
   }
+  // If there is no caption track, use detect to get the language code from the text
   const text = [title, description, author].join(" ");
+  // Remove anything that is not a letter or a space in any language
   const cleanText = text
     .replace(/https?:\/\/\S+/g, "")
     .replace(/[^\p{L}\s]/gu, "")
@@ -32,6 +38,7 @@ async function getLanguage(player, response, title, description, author) {
   return await detect(cleanText);
 }
 
+// Get the video data from the player
 async function getYTVideoData() {
   const player = document.querySelector("#movie_player");
   const data = player.getVideoData();
