5861d7075406254a98777e02c6fc97b2807f5c65
unknown <toil.official@yandex.com>
2023-08-17 16:57:18 +0300

    fix eslint node

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true,
         "greasemonkey": true,
-        "node ": true
+        "node": true
     },
     extends: "eslint:recommended",
     overrides: [
