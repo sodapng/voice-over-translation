df4b8f5794ea0fcef0e20a85e1dc7daf27b573b1
unknown <toil.official@yandex.com>
2023-10-07 15:18:49 +0300

    added m3u8 proxy host

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 // CONFIGURATION
 const workerHost = "api.browser.yandex.ru";
+const m3u8ProxyHost = "m3u8proxy.toil-dump.workers.dev";
 const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 const yandexUserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.1.1140 Yowser/2.5 Safari/537.36";
 const autoVolume = 0.15; // 0.0 - 1.0 (0% - 100%) - default volume of the video with the translation
 
-export { workerHost, yandexHmacKey, yandexUserAgent, autoVolume };
+export { workerHost, m3u8ProxyHost, yandexHmacKey, yandexUserAgent, autoVolume };
