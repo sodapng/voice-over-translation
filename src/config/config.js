6023ddf17904c0515ee246ccb6b5c0f6af1e4975
Toil <ilya.t24@yandex.ru>
2023-12-06 23:27:18 +0300

    added some detect & translate vars

--- src/config/config.js
+++ src/config/config.js
@@ -5,10 +5,25 @@ const yandexHmacKey = "xtGCyGdTY2Jy6OMEKdTuXev3Twhkamgm";
 const yandexUserAgent =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 YaBrowser/23.7.1.1140 Yowser/2.5 Safari/537.36";
 const autoVolume = 0.15; // 0.0 - 1.0 (0% - 100%) - default volume of the video with the translation
+const defaultTranslationService = "yandex";
+const defaultDetectService = "yandex";
+
+const detectUrls = {
+  yandex: "https://translate.toil.cc/detect",
+  rustServer: "https://rust-server-531j.onrender.com/detect",
+};
+
+const translateUrls = {
+  yandex: "https://translate.toil.cc/translate",
+};
 
 export {
   workerHost,
   m3u8ProxyHost,
+  detectUrls,
+  translateUrls,
+  defaultTranslationService,
+  defaultDetectService,
   yandexHmacKey,
   yandexUserAgent,
   autoVolume,
